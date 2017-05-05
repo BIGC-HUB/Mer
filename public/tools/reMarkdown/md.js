8f1ed07407a14f60c29783aeaaa503f961dfcfe0
大海 <iwangyang@qq.com>
2017-05-05 14:58:17 +0800

    酷炫

--- public/tools/reMarkdown/md.js
+++ public/tools/reMarkdown/md.js
@@ -46,49 +46,64 @@ let bindEvent = function() {
         if (edit && !bool) {
             this.dataset.edit = true
             e.preventDefault()
-
             let index = $(this).attr('name').split('#')[1]
             let json = localStorage.md || '["# new"]'
             let arr = JSON.parse(json)
             let val = arr[index] || ''
-            $(this).html('')
-            let editor = CodeMirror(this, {
-                value: val,
-                mode: 'gfm',
-                lineNumbers: true,
-                // 换行
-                lineWiseCopyCut: true,
-                lineWrapping: 'wrap',
-                theme: "material"
-            })
-            editor.focus()
+            $(this).html('<div id="editor"></div>')
             $(this).append('<button class="btn btn-new"> + </button>')
-            editor.on('input', function(e) {
-                log(123)
+            // 引入代码补全和提示模块
+            ace.require("ace/ext/language_tools");
+            // 创建编辑器
+            md.editor = ace.edit("editor")
+            md.editor.$blockScrolling = Infinity
+            md.editor.setValue(val)
+            md.editor.setOptions({
+                // 高度自适应
+                maxLines: 40,
+                // 是否自动补全 联想提示
+                enableBasicAutocompletion: true,
+                enableSnippets: true,
+                enableLiveAutocompletion: true,
+                // 折叠
+                showFoldWidgets: false,
+                // 模式
+                mode: "ace/mode/markdown",
+                autoScrollEditorIntoView: true
             })
-            editor.on('blur', function(e) {
-                let parent = e.display.wrapper.parentElement
-                let value = e.getValue()
-                parent.dataset.edit = ''
-                let id = parseInt($(parent).attr('name').split('#')[1])
-                let json = localStorage.md || '["# new"]'
-                let arr = JSON.parse(json)
-                if (onlyNone(value)) {
-                    arr.splice(id, 1)
-                    let c = $('.c')
-                    for (let i = id + 1; i < c.length; i++) {
-                        $(c[i]).attr('name', 'c#' + (i - 1))
-                    }
-                    c[id].remove()
-                } else {
-                    arr[id] = value
-                }
-                localStorage.md = JSON.stringify(arr)
-                // Show Html
-                $(parent).html(md.render(value))
-            })
+            md.editor.setTheme("ace/theme/tomorrow_night")
+            // 未知
+            md.editor.renderer.setPrintMarginColumn(false)
+            md.editor.session.setNewLineMode("unix")
+            // 自动换行
+            md.editor.session.setUseWrapMode(true)
+            // 获得焦点
+            md.editor.gotoLine(1)
+            md.editor.renderer.textarea.focus()
         }
     })
+    // hide
+    // $('#md').on('blur', '#editor', function() {
+    //     let parent = this.parentElement
+    //     let value = md.editor.getValue()
+    //     parent.dataset.edit = ''
+    //     let id = parseInt($(parent).attr('name').split('#')[1])
+    //     let json = localStorage.md || '["# new"]'
+    //     let arr = JSON.parse(json)
+    //     if (onlyNone(value)) {
+    //         arr.splice(id, 1)
+    //         let c = $('.c')
+    //         for (let i = id + 1; i < c.length; i++) {
+    //             $(c[i]).attr('name', 'c#' + (i - 1))
+    //         }
+    //         c[id].remove()
+    //     } else {
+    //         arr[id] = value
+    //     }
+    //     localStorage.md = JSON.stringify(arr)
+    //     // Show Html
+    //     $(parent).html(md.render(value))
+    // })
     // btn-new
     $('#md').on('mousedown', '.btn-new', function(e) {
         let parent = this.parentElement
